b0a596976deb3f4afd07f299b1b414c024c2dc13
Josh Comeau <joshwcomeau@gmail.com>
2015-02-06 12:58:18 -0500

    Added seinfeld pic. Further styling.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
   
-  $(".line").percentext();
+  $(".line.vbig").percentext({
+    width: 90,
+    letterSpacing: -20
+  });
+  $(".line.big").percentext({
+    width: 90,
+    letterSpacing: -10
+  });
 
+  $(".line.norm").percentext({
+    width: 90,
+    letterSpacing: -5
+  });
+
+  $(".line.small").percentext({
+    width: 90
+  });
+
+  $(".line.vsmall").percentext({
+    width: 90,
+    letterSpacing: 5
+  });
 });
