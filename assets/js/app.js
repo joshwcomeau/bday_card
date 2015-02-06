49b002bde907aaeaf539127253ce388d50537c55
Josh Comeau <joshwcomeau@gmail.com>
2015-02-06 13:43:49 -0500

    Finished the seinfeld quote text

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){
   
   $(".line.vbig").percentext({
-    width: 90,
+    width: 80,
     letterSpacing: -20
   });
   $(".line.big").percentext({
-    width: 90,
+    width: 80,
     letterSpacing: -10
   });
 
   $(".line.norm").percentext({
-    width: 90,
+    width: 80,
     letterSpacing: -5
   });
 
   $(".line.small").percentext({
-    width: 90
+    width: 80
   });
 
   $(".line.vsmall").percentext({
-    width: 90,
+    width: 80,
     letterSpacing: 5
   });
 });
